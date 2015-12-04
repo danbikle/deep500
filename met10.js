c482db80fd89420d32fee0412f918cd89bd34e0b
Dan Bikle <dan.bikle@gmail.com>
2015-12-04 21:51:51 +0000

    mo

--- met10.js
+++ met10.js
@@ -4,22 +4,19 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      // this function depends on Session value for hideCompleted
-      if(Session.get("hideCompleted")) {
+      if (Session.get("hideCompleted")) {
         // If hide completed is checked, filter tasks
         return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-      } else { // Return all tasks
+      } else {
+        // Otherwise, return all of the tasks
         return Tasks.find({}, {sort: {createdAt: -1}});
       }
     },
     hideCompleted: function () {
-      return Session.get('hideCompleted');
-    }
-  });
-
-  Template.header.events({
-    "change .hide-completed input": function(event){
-      Session.set('hideCompleted', 'event.target.checked')
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count()
     }
   });
 
@@ -39,15 +36,22 @@ if (Meteor.isClient) {
 
       // Clear form
       event.target.text.value = "";
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
   Template.task.events({
     "click .toggle-checked": function () {
-      // This should toggle value of checked-property:
-      Tasks.update(this._id, {$set: {checked: ! this.checked}})
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
     },
-    "click button.delete": function(){Tasks.remove(this._id)}
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
   });
 }
 
