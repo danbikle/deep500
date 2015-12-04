7c46e2c20a3f5fc65eef091f472e76b26f489e5f
Dan Bikle <dan.bikle@gmail.com>
2015-12-04 18:34:37 +0000

    mo

--- met10.js
+++ met10.js
@@ -1,33 +1,38 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var text = event.target.text.value;
+
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
     }
+  });
+
+  Template.task.events({
+    "click .toggle-checked": function () {
+      // This should toggle value of checked-property:
+      Tasks.update(this._id, {$set: {checked: ! this.checked}})
+    },
+    "click button.delete": function(){Tasks.remove(this._id)}
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-      'hello';
-      'hello';
-      'hello';
- debugger;
-      var dan = 1;
-      var danny = 2;
-      var daniel = 3;
-      'hello';
-      'hello';
-      'hello';
-  });
-}
