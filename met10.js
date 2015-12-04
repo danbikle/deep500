d58a6de33bce144ebfa96240da59d8974b443b77
Dan Bikle <dan.bikle@gmail.com>
2015-12-04 06:50:40 +0000

    hi

---
+++ met10.js
@@ -0,0 +1,33 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+      'hello';
+      'hello';
+      'hello';
+ debugger;
+      var dan = 1;
+      var danny = 2;
+      var daniel = 3;
+      'hello';
+      'hello';
+      'hello';
+  });
+}
