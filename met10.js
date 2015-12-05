f3f8cad5d29272746c2e198b49b50fcf83e24d8c
Dan Bikle <dan.bikle@gmail.com>
2015-12-05 22:27:01 +0000

    mo

--- met10.js
+++ met10.js
@@ -3,23 +3,16 @@ Mmodels = new Mongo.Collection("mmodels");
 if (Meteor.isClient) {
   // This code only runs on the client
 
-  Session.set("building", false);
   Session.set("stopped",  true);
 
   Template.body.helpers({
-    building: function(){return Session.get("building")},
-    stopped: function(){return  Session.get("stopped" )}
+    stopped: function(){return Session.get("stopped" )}
   });
 
   Template.body.events({
     "click #startbutton": function () {
-      // I should hide the button
-      // d3.select('#a_waiter').remove()
-      // I should start building model 
-      Session.set("building", true);
-      Session.set("stopped",  false);
+      Session.set("stopped", false) // building now.
       start_modelbuild()
-      'done'
     },
   });
 
@@ -27,7 +20,7 @@ if (Meteor.isClient) {
     passwordSignupFields: "USERNAME_ONLY"
   });
 
-}
+} // if (Meteor.isClient)
 
 // This code runs both on client and server
 Meteor.methods({
