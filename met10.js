d12ff9634fca7424574558fa501b3c0a0ae76d18
Dan Bikle <dan.bikle@gmail.com>
2015-12-05 01:59:00 +0000

    mo

--- met10.js
+++ met10.js
@@ -31,7 +31,9 @@ if (Meteor.isClient) {
       // Insert a task into the collection
       Tasks.insert({
         text: text,
-        createdAt: new Date() // current time
+        createdAt: new Date(),            // current time
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username  // username of logged in user
       });
 
       // Clear form
@@ -53,5 +55,7 @@ if (Meteor.isClient) {
       Tasks.remove(this._id);
     }
   });
-}
 
+  Accounts.ui.config({passwordSignupFields: "USERNAME_ONLY"})
+
+}
