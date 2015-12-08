da766374fecdc7bbfebc48dc77f407df97b15a95
Dan Bikle <dan.bikle@gmail.com>
2015-12-08 18:00:20 +0000

    mo

--- met10.js
+++ met10.js
@@ -1,10 +1,11 @@
 Tasks = new Mongo.Collection("tasks");
+Mmodels = new Mongo.Collection("mmodels");
 
 if (Meteor.isServer) {
   // This code only runs on the server
-  // Only publish tasks that are public or belong to the current user
-  Meteor.publish("tasks", function () {
-    return Tasks.find({
+  // Only publish mmodels that are public or belong to the current user
+  Meteor.publish("mmodels", function () {
+    return Mmodels.find({
       $or: [
         { private: {$ne: true} },
         { owner: this.userId }
@@ -15,36 +16,36 @@ if (Meteor.isServer) {
 
 if (Meteor.isClient) {
   // This code only runs on the client
-  Meteor.subscribe("tasks");
+  Meteor.subscribe("mmodels");
 
   Template.body.helpers({
-    tasks: function () {
+    mmodels: function () {
       if (Session.get("hideCompleted")) {
-        // If hide completed is checked, filter tasks
-        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+        // If hide completed is checked, filter mmodels
+        return Mmodels.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
       } else {
-        // Otherwise, return all of the tasks
-        return Tasks.find({}, {sort: {createdAt: -1}});
+        // Otherwise, return all of the mmodels
+        return Mmodels.find({}, {sort: {createdAt: -1}});
       }
     },
     hideCompleted: function () {
       return Session.get("hideCompleted");
     },
     incompleteCount: function () {
-      return Tasks.find({checked: {$ne: true}}).count();
+      return Mmodels.find({checked: {$ne: true}}).count();
     }
   });
 
   Template.body.events({
-    "submit .new-task": function (event) {
+    "submit .new-mmodel": function (event) {
       // Prevent default browser form submit
       event.preventDefault();
 
       // Get value from form element
       var text = event.target.text.value;
 
-      // Insert a task into the collection
-      Meteor.call("addTask", text);
+      // Insert a mmodel into the collection
+      Meteor.call("addMmodel", text);
 
       // Clear form
       event.target.text.value = "";
@@ -54,19 +55,19 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.task.helpers({
+  Template.mmodel.helpers({
     isOwner: function () {
       return this.owner === Meteor.userId();
     }
   });
 
-  Template.task.events({
+  Template.mmodel.events({
     "click .toggle-checked": function () {
       // Set the checked property to the opposite of its current value
       Meteor.call("setChecked", this._id, ! this.checked);
     },
     "click .delete": function () {
-      Meteor.call("deleteTask", this._id);
+      Meteor.call("deleteMmodel", this._id);
     },
     "click .toggle-private": function () {
       Meteor.call("setPrivate", this._id, ! this.private);
@@ -79,45 +80,45 @@ if (Meteor.isClient) {
 }
 
 Meteor.methods({
-  addTask: function (text) {
-    // Make sure the user is logged in before inserting a task
+  addMmodel: function (text) {
+    // Make sure the user is logged in before inserting a mmodel
     if (! Meteor.userId()) {
       throw new Meteor.Error("not-authorized");
     }
 
-    Tasks.insert({
+    Mmodels.insert({
       text: text,
       createdAt: new Date(),
       owner: Meteor.userId(),
       username: Meteor.user().username
     });
   },
-  deleteTask: function (taskId) {
-    var task = Tasks.findOne(taskId);
-    if (task.private && task.owner !== Meteor.userId()) {
-      // If the task is private, make sure only the owner can delete it
+  deleteMmodel: function (mmodelId) {
+    var mmodel = Mmodels.findOne(mmodelId);
+    if (mmodel.private && mmodel.owner !== Meteor.userId()) {
+      // If the mmodel is private, make sure only the owner can delete it
       throw new Meteor.Error("not-authorized");
     }
 
-    Tasks.remove(taskId);
+    Mmodels.remove(mmodelId);
   },
-  setChecked: function (taskId, setChecked) {
-    var task = Tasks.findOne(taskId);
-    if (task.private && task.owner !== Meteor.userId()) {
-      // If the task is private, make sure only the owner can check it off
+  setChecked: function (mmodelId, setChecked) {
+    var mmodel = Mmodels.findOne(mmodelId);
+    if (mmodel.private && mmodel.owner !== Meteor.userId()) {
+      // If the mmodel is private, make sure only the owner can check it off
       throw new Meteor.Error("not-authorized");
     }
 
-    Tasks.update(taskId, { $set: { checked: setChecked} });
+    Mmodels.update(mmodelId, { $set: { checked: setChecked} });
   },
-  setPrivate: function (taskId, setToPrivate) {
-    var task = Tasks.findOne(taskId);
+  setPrivate: function (mmodelId, setToPrivate) {
+    var mmodel = Mmodels.findOne(mmodelId);
 
-    // Make sure only the task owner can make a task private
-    if (task.owner !== Meteor.userId()) {
+    // Make sure only the mmodel owner can make a mmodel private
+    if (mmodel.owner !== Meteor.userId()) {
       throw new Meteor.Error("not-authorized");
     }
 
-    Tasks.update(taskId, { $set: { private: setToPrivate } });
+    Mmodels.update(mmodelId, { $set: { private: setToPrivate } });
   }
 });
