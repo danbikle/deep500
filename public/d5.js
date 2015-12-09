8644f0c3213cb7b2148c7ff9007c5a6dc4118fe5
Dan Bikle <dan.bikle@gmail.com>
2015-12-09 05:59:03 +0000

    mo

--- public/d5.js
+++ public/d5.js
@@ -6,7 +6,7 @@ but it takes a L O O O N G time to run.
 
 // This function should start model build
 function start_modelbuild(){
-  d3.select('#divspinner').html('<span id='a_spinner'>I am a spinner.</span>')
+  d3.select('#divspinner').html('<span id="a_spinner">I am a spinner.</span>')
   d3.csv("/csv/GSPC.csv", cb1)
 }
 
