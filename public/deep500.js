1848badce179160853b181d283630d9c44579f74
Dan Bikle <dan.bikle@gmail.com>
2015-12-14 19:27:00 +0000

    more

--- public/deep500.js
+++ public/deep500.js
@@ -106,21 +106,22 @@ function calc_results(predictions_a,labels_oos_a,pctlead_oos_a){
   results_o.falsepos     = falsepos
   results_o.trueneg      = trueneg
   results_o.falseneg     = falseneg
-  results_o.pos_accuracy = pos_accuracy
-  results_o.neg_accuracy = neg_accuracy
-  results_o.accuracy     = accuracy
-  results_o.pos_avg      = pos_avg
-  results_o.neg_avg      = neg_avg
+  results_o.pos_accuracy = d3.round(pos_accuracy,1)
+  results_o.neg_accuracy = d3.round(neg_accuracy,1)
+  results_o.accuracy     = d3.round(accuracy    ,3)
+  results_o.pos_avg      = d3.round(pos_avg     ,3)
+  results_o.neg_avg      = d3.round(neg_avg     ,3)
   if (pos_avg > neg_avg)
     results_o.opinion    = 'good'
   else
     results_o.opinion    = 'bad';
-
+  // I should start work on chart data
   var ydate_a   = model_o.ydate_a
   var oos_end   = ydate_a.length
   var oos_start = oos_end - oos_size
   var ydate_oos_a  = ydate_a.slice(oos_start,oos_end)
   var cp_oos_a     = model_o.cp_a.slice(oos_start,oos_end)
+  // RickShaw should use blue_a later to create chart
   results_o.blue_a = []
   for(dy=0; dy<oos_size; dy++){
     results_o.blue_a.push({x:ydate_oos_a[dy], y:cp_oos_a[dy]})
