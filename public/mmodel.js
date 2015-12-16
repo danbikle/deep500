d1e61e0419013d6af5d33e8c84f5fcbe4dad099a
Dan Bikle <dan.bikle@gmail.com>
2015-12-16 02:45:39 +0000

    more

--- public/mmodel.js
+++ public/mmodel.js
@@ -113,7 +113,8 @@ function cr_mn(train_o) {
     if (json_state == 'need json'){
       model_o.mnjson    = magicNet.toJSON()
       json_state        = 'have json'
-      model_o.build_duration = d3.round((Date.now()-mn_start)/60.0/1000.0,3)
+      model_o.build_date     = Date.now()
+      model_o.build_duration = d3.round((model_o.build_date-mn_start)/60.0/1000.0,3)
       predict_o.mymn    = magicNet
       model_o.results_o = predict_oos(predict_o)
       Meteor.call("addMmodel", model_o)
