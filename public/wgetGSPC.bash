b5e4b60ab61b690c8b06e646624f01ec1be913f6
Dan Bikle <dan.bikle@gmail.com>
2015-12-29 03:38:24 +0000

    more

--- public/wgetGSPC.bash
+++ public/wgetGSPC.bash
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # ~/d5/public/wgetGSPC.bash
+# This script depends on Anaconda Python which I get here:
+# https://www.continuum.io/downloads
 
 # This script should get prices at 12:50 Calif time.
+# See crontab demo here:  crontab.txt
+
 . ${HOME}/d5/public/d5env.bash
 
 # cd ${HOME}/d5/public
@@ -16,7 +20,7 @@ rm -f ${TKR}.csv ${TKR}.html
 wget --output-document=${TKR}.csv http://ichart.finance.yahoo.com/table.csv?s=${TKRH}
 cat ${TKR}.csv | awk -F, '{print $1 "," $5}' > ${TKR}2.csv
 wget --output-document=${TKR}.html http://finance.yahoo.com/q?s=$TKRH
-# I should extract recent prices from html
+# I should extract recent prices from html using Anaconda Python:
 python ${D5}/csv/extprice.py
 # I should cat prices together
 cat ${TKR}recent.csv ${TKR}2.csv|grep -v Date > ${TKR}3.csv
