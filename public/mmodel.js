b98a76cc811a5c8ec0b57ea057cc0c9af7dbe316
Dan Bikle <dan.bikle@gmail.com>
2015-12-29 02:54:47 +0000

    more

--- public/mmodel.js
+++ public/mmodel.js
@@ -74,18 +74,24 @@ function start_modelbuild(){
 // debug
 
 function mergem(csv_0, d5_recent_prices_a){
+  var rprices_a = d5_recent_prices_a
+  // I should look for duplicate price and remove it if I see it.
+  var dp0 = d5_recent_prices_a[0]
+  var dp1 = d5_recent_prices_a[1]
+  if(dp0[0] == dp1[1])
+    rprices_a = d5_recent_prices_a.slice(1,d5_recent_prices_a.length)
+//    rprices_a = d5_recent_prices_a.slice(1,d5_recent_prices_a.length-1)
   csv_0
-  d5_recent_prices_a.reverse()
-  d5_recent_prices_a.forEach(function(arow){
+  rprices_a.reverse()
+  rprices_a.forEach(function(arow){
     arow
-    for(rw=0; rw<csv_0.length; rw++){
-      arow
-      brow = csv_0[rw]
-      brow['Date']
-    }
+    // I should add arow to csv_0 if arow has a later date than last csv_0
+    if(arow[0] > csv_0[csv_0.length-1]['Date'])
+      csv_0[csv_0.length] = {'Date':arow[0],'Close':arow[1]}
+    csv_0[csv_0.length-1]
   })
 
-}
+} //function mergem()
 
 // debug
 
