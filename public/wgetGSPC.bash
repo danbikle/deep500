0837c102cb9dc75d7561a3e457d8385a83bb549e
Dan Bikle <dan.bikle@gmail.com>
2015-12-29 01:21:54 +0000

    more

--- public/wgetGSPC.bash
+++ public/wgetGSPC.bash
@@ -19,14 +19,14 @@ wget --output-document=${TKR}.html http://finance.yahoo.com/q?s=$TKRH
 # I should extract recent prices from html
 python ${D5}/csv/extprice.py
 # I should cat prices together
-echo 'cdate,cp'                                > ${TKR}3.csv
-cat ${TKR}recent.csv ${TKR}2.csv|grep -v Date >> ${TKR}3.csv
-cat ${TKR}3.csv                                > ${TKR}2.csv 
+cat ${TKR}recent.csv ${TKR}2.csv|grep -v Date > ${TKR}3.csv
+cat ${TKR}3.csv                               > ${TKR}2.csv 
 
-echo 'var d5_recent_prices_s = "' > d5rp.js
-head GSPC2.csv                   >> d5rp.js
-echo '"'                         >> d5rp.js
-cat                                 d5rp.js
+echo 'var d5_recent_prices_s = ['   > d5rp.js
+head GSPC2.csv|awk -F, "{print $1 $2}"|\
+  sed '1,$s/^/"/'|sed '1,$s/,/",/' >> d5rp.js
+echo ']'                           >> d5rp.js
+cat                                   d5rp.js
 set -x
 edate=`date '+%s'`
 cd $D5
